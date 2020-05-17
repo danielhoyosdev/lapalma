246e36c0d5686663eac47c659e4a370cd3fabf18
Daniel Hoyos <danielhoyos350@gmail.com>
2020-05-16 19:01:09 -0500

    Updates

---
+++ precache-manifest.89da9131cae338f06e5f18d222d98fa0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f6195fe54646a0d6092bcba488cb61",
+    "url": "/lapalma/index.html"
+  },
+  {
+    "revision": "c56c875cc28fbfcc9082",
+    "url": "/lapalma/static/css/main.fdce71ca.chunk.css"
+  },
+  {
+    "revision": "fca6218bf3714552cb1e",
+    "url": "/lapalma/static/js/2.bc3db5e5.chunk.js"
+  },
+  {
+    "revision": "45c3b0e6a77e536ad25f6ffa575bb86a",
+    "url": "/lapalma/static/js/2.bc3db5e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56c875cc28fbfcc9082",
+    "url": "/lapalma/static/js/main.a1df1e8f.chunk.js"
+  },
+  {
+    "revision": "658923949390f91872ff",
+    "url": "/lapalma/static/js/runtime-main.bb396262.js"
+  },
+  {
+    "revision": "db61e6e368ae87ca675b219ca204d64d",
+    "url": "/lapalma/static/media/Servicio1.db61e6e3.jpg"
+  },
+  {
+    "revision": "6351a2bdc6874dc01ae1536839250ade",
+    "url": "/lapalma/static/media/Servicio2.6351a2bd.jpg"
+  },
+  {
+    "revision": "4fcab89aafd0f8a9ea37d0dc440a597b",
+    "url": "/lapalma/static/media/Servicio3.4fcab89a.jpg"
+  },
+  {
+    "revision": "c3788b9777ea21b4e08ffae695e24f72",
+    "url": "/lapalma/static/media/Servicio4.c3788b97.jpg"
+  },
+  {
+    "revision": "11d11b4a9d794f5bf1b2755d90f3c983",
+    "url": "/lapalma/static/media/background-contacto.11d11b4a.jpg"
+  },
+  {
+    "revision": "cbfd56b543d0e274e2d7570d520eb3b8",
+    "url": "/lapalma/static/media/background-movil.cbfd56b5.png"
+  },
+  {
+    "revision": "4e833f7642f00c0b2e1cbe656240d7e8",
+    "url": "/lapalma/static/media/background.4e833f76.png"
+  },
+  {
+    "revision": "4ad43290d25df00ae35d18207418b113",
+    "url": "/lapalma/static/media/cliente1.4ad43290.png"
+  },
+  {
+    "revision": "c893132928e9dffc770eab9faa0f98d7",
+    "url": "/lapalma/static/media/cliente2.c8931329.png"
+  },
+  {
+    "revision": "e1d879f89a1215cb03f37d1439c8a0e6",
+    "url": "/lapalma/static/media/cliente3.e1d879f8.png"
+  },
+  {
+    "revision": "a7570a661d5521521a5a1e5378f9de33",
+    "url": "/lapalma/static/media/cliente4.a7570a66.png"
+  },
+  {
+    "revision": "d8d276167585cb3abc220948ac0aa64b",
+    "url": "/lapalma/static/media/logo-white.d8d27616.png"
+  },
+  {
+    "revision": "a198c24ab40fd14046460fd28140785d",
+    "url": "/lapalma/static/media/logo.a198c24a.png"
+  },
+  {
+    "revision": "dc9c3a8004985c81d08073ba1bb029c3",
+    "url": "/lapalma/static/media/nosotros.dc9c3a80.jpg"
+  },
+  {
+    "revision": "6590ae7eb247829371dc8aa8872e5c22",
+    "url": "/lapalma/static/media/opcion1.6590ae7e.png"
+  },
+  {
+    "revision": "3de5656606788c7f78161e082e0407a0",
+    "url": "/lapalma/static/media/opcion2.3de56566.png"
+  },
+  {
+    "revision": "00adeff7389aef86ba39193aeddcd8cb",
+    "url": "/lapalma/static/media/opcion3.00adeff7.png"
+  },
+  {
+    "revision": "aacd486de91169fa5331edf78072a9ab",
+    "url": "/lapalma/static/media/opcion4.aacd486d.png"
+  }
+]);
